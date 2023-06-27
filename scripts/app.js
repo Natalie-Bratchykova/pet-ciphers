70ed33666ef714b52fbc0940bc0ecffd9883045b
Natalie-Bratchykova <nataliya.bratchykova@gmail.com>
2023-06-27 23:16:46 +0300

    added interaction for caesar, thrithemius ciphers

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,20 @@
 import CaesarCipher from "./caesar.js";
+import ThrithemiusCipher from "./Trithemius.js";
+import xorCipher from "./XOREncryption.js";
+import desCipher from "./des.js";
+import aesCipher from "./aes.js";
+import knapsackCipher from "./knapsack.js";
+import rsaCipher from "./RSA.js";
+const caesar = new CaesarCipher();
+const thrithemius = new ThrithemiusCipher();
+const xor = new xorCipher();
+const des = new desCipher();
+const aes = new aesCipher();
+const knapsack = new knapsackCipher();
+const rsa = new rsaCipher();
+
+const radioAsString = document.querySelector("#string");
+const radioAsNumber = document.querySelector("#number");
 const btnSetColorTheme = document.querySelector("#colorTheme");
 const popupSetColorTheme = document.querySelector(".color-theme");
 const overlay = document.querySelector(".overlay");
@@ -13,9 +29,14 @@ const inputColorTextareaText = document.querySelector("#textareaText");
 const inputColorButtonsBg = document.querySelector("#buttonsBg");
 const inputColorButtonsText = document.querySelector("#buttonsText");
 
+const thrithemiusKeySection = document.querySelector(".thrithemius");
+const standartedCiphers = document.querySelector(".standartedCiphers");
 const title = document.querySelector(".title");
 const selectCipherType = document.querySelector("#ciphersDropDown");
 const allCipherOptions = document.querySelectorAll(".dropdown-item");
+const btngenerateKeyPairs = document.querySelector("#generatePublicKey");
+const knapsackKey = document.querySelector(".knapsack");
+const paralellKey = document.querySelector(".paralell-key__input");
 let r = document.querySelector(":root");
 
 const keyInput = document.querySelector("#key");
@@ -31,23 +52,27 @@ allButtons.forEach((btn) =>
   })
 );
 class App {
+  static ciphers = {
+    caesar: "Caesar",
+    thrithemius: "Thrithemius",
+    xor: "XOR",
+    des: "DES",
+    aes: "AES",
+    knapsack: "Knapsack",
+    rsa: "RSA",
+  };
   constructor() {
-    this.ciphers = {
-      caesar: "Caesar",
-      thrithemius: "Thrithemius",
-      xor: "XOR",
-      des: "DES",
-      aes: "AES",
-      knapsack: "Knapsack",
-      rsa: "RSA",
-    };
     this.initialize();
   }
+
   initialize() {
     this.openThemeSettings();
     this.closeThemeSettings();
     this.applyThemeSettings();
     this.setCipherTitle();
+    this.encrypt();
+    this.decrypt();
+    this.defineKeyType();
   }
 
   openThemeSettings() {
@@ -88,43 +113,81 @@ class App {
     allCipherOptions.forEach((option) => {
       option.addEventListener("click", function (e) {
         title.textContent = e.target.dataset.cipher;
+        if (title.textContent === App.ciphers.thrithemius) {
+          thrithemiusKeySection.classList.remove("--hiden");
+        } else {
+          thrithemiusKeySection.classList.add("--hiden");
+        }
+        if (
+          title.textContent === App.ciphers.aes ||
+          title.textContent === App.ciphers.des
+        ) {
+          standartedCiphers.classList.remove("--hiden");
+        } else {
+          standartedCiphers.classList.add("--hiden");
+        }
+        if (title.textContent === App.ciphers.knapsack) {
+          paralellKey.classList.add("--hiden");
+          knapsackKey.classList.remove("--hiden");
+          btngenerateKeyPairs.classList.remove("--hiden");
+        } else {
+          paralellKey.classList.remove("--hiden");
+          knapsackKey.classList.add("--hiden");
+          btngenerateKeyPairs.classList.add("--hiden");
+        }
       });
-      this.defineCipher();
     });
   }
 
-  defineCipher() {
-    if (title.textContent === this.ciphers.caesar) {
-      this.doCaesarCipher();
-    } else {
-      console.log("lolololalalala");
-    }
+  // executes 2 time
+  encrypt() {
+    let clicked = 0;
+    btnEncrypt.addEventListener("click", function (e) {
+      clicked++;
+      e.preventDefault();
+      if (title.textContent === App.ciphers.caesar) {
+        textarea.value = caesar.encrypt(textarea.value, Number(keyInput.value));
+      } else if (title.textContent === App.ciphers.thrithemius) {
+        let isString = radioAsString.checked ? true : false;
+        textarea.value = thrithemius.encrypt(
+          textarea.value,
+          keyInput.value,
+          isString
+        );
+      } else if (title.textContent === App.ciphers.xor) {
+        let gamma = xor.generateGamma(keyInput.value, textarea.value);
+        textarea.value = xor.doCipher(textarea.value, gamma);
+      }
+    });
   }
 
-  doCaesarCipher() {
-    const caesar = new CaesarCipher();
-    btnEncrypt = this.deleteAllEventsListener(btnEncrypt);
-    btnDecrypt = this.deleteAllEventsListener(btnDecrypt);
-    btnEncrypt.addEventListener("click", () => {
-      alert("encrypt");
-      textarea.value = caesar.encrypt(textarea.value, Number(keyInput.value));
-    });
-    btnDecrypt.addEventListener("click", () => {
-      alert("decrypt");
-      textarea.value = caesar.decrypt(textarea.value, Number(keyInput.value));
+  decrypt() {
+    btnDecrypt.addEventListener("click", function (e) {
+      e.preventDefault();
+      if (title.textContent === App.ciphers.caesar) {
+        textarea.value = caesar.decrypt(textarea.value, Number(keyInput.value));
+      } else if (title.textContent === App.ciphers.thrithemius) {
+        let isString = radioAsString.checked ? true : false;
+        textarea.value = thrithemius.decrypt(
+          textarea.value,
+          keyInput.value,
+          isString
+        );
+      }
     });
   }
 
-  // doesn't work
-  deleteAllEventsListener(el) {
-    return el.cloneNode(true);
+  defineKeyType() {
+    keyInput.addEventListener("input", function (e) {
+      if (/^-?[0-9]+$/.test(keyInput.value.replaceAll(" ", ""))) {
+        radioAsNumber.disabled = false;
+      } else {
+        radioAsNumber.disabled = true;
+      }
+    });
   }
 }
 
 const app = new App();
 app.initialize();
-allCipherOptions.forEach((option) => {
-  option.addEventListener("click", function () {
-    app.defineCipher();
-  });
-});
+//
